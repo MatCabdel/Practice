2675486e556bafba041a7b7587c622810244dc77
Mathieu Chauveau <chauveau.math@gmail.com>
2024-09-24 16:50:32 +0200

    practice atelier

--- Typescript/Atelier/practice1.js
+++ Typescript/Atelier/practice1.js
@@ -4,3 +4,25 @@ function stringifyMessage(text, createdAt) {
     return "".concat(text, ", the ").concat(createdAt.toLocaleDateString());
 }
 console.log(stringifyMessage(messageText, messageCreatedAt));
+var message1 = { from: "Bob", message: "Hello Alice!", isRead: true };
+var message2 = { from: "Alice", message: "Hi Bob!" };
+var messages = [message1, message2]; // type this line
+function readMessages(messages) {
+    // put some logic to set isRead to true on all messages
+    return 0;
+}
+console.log(readMessages(messages));
+var notification1 = { at: 1694011133, state: "unread" };
+var notification2 = { at: 1694011532, state: "read" };
+function readNotificationOrMessage(notificationOrMessage) {
+    if ("isRead" in notificationOrMessage) {
+        notificationOrMessage.isRead = true;
+    }
+    else if ("state" in notificationOrMessage) {
+        notificationOrMessage.state = "read";
+    }
+}
+readNotificationOrMessage(message2);
+readNotificationOrMessage(notification2);
+console.log("le message lu:", message2);
+console.log("info sur la notif:", notification2);
--- Typescript/Atelier/practice1.ts
+++ Typescript/Atelier/practice1.ts
@@ -6,3 +6,42 @@ function stringifyMessage(text: string, createdAt: Date): string { // this one t
 }
 
 console.log(stringifyMessage(messageText, messageCreatedAt));
+
+type Message = {
+  from: string;
+  message: string;
+  isRead?: boolean;
+}
+
+const message1: Message = { from: "Bob", message: "Hello Alice!", isRead: true };
+const message2: Message = { from: "Alice", message: "Hi Bob!" };
+
+const messages: Message [] = [message1, message2]; // type this line
+function readMessages(messages: Message []) { // this one too
+	// put some logic to set isRead to true on all messages
+	return 0;
+}
+console.log(readMessages(messages));
+
+type Notif = {
+  at: number;
+  state: string;
+}
+
+const notification1: Notif = { at: 1694011133, state: "unread" };
+const notification2: Notif = { at: 1694011532, state: "read" };
+
+function readNotificationOrMessage(notificationOrMessage: Message | Notif): void { // type this line
+	if ("isRead" in notificationOrMessage) {
+		notificationOrMessage.isRead = true; 
+	} else if ("state" in notificationOrMessage) { 
+		notificationOrMessage.state = "read"; 
+	}
+}
+
+readNotificationOrMessage(message2); 
+readNotificationOrMessage(notification2); 
+
+console.log("le message lu:", message2); 
+console.log("info sur la notif:", notification2); 
+
